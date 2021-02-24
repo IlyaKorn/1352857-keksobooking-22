12e885d63b20b997b6691659db0c66f4188ef4c6
Ilya Koren <ilyakoren@mail.ru>
2021-02-24 17:52:51 +0300

    Повторные исправления замечаний

--- js/validation-form.js
+++ js/validation-form.js
@@ -14,22 +14,19 @@ const checkRoomsPlaceForm = function () {
   if (this.value === '1') {
     quantityGuestsPlaceForm.options[2].removeAttribute('disabled');
     quantityGuestsPlaceForm.value = '1';
-  } else if (this.value === '2' && quantityGuestsPlaceForm.value === '1' || this.value === '2' && quantityGuestsPlaceForm.value === '2') {
-    quantityGuestsPlaceForm.options[1].removeAttribute('disabled');
-    quantityGuestsPlaceForm.options[2].removeAttribute('disabled');
   } else if (this.value === '2') {
     quantityGuestsPlaceForm.options[1].removeAttribute('disabled');
     quantityGuestsPlaceForm.options[2].removeAttribute('disabled');
-    quantityGuestsPlaceForm.value = '2';
-  } else if (this.value === '3' && quantityGuestsPlaceForm.value === '1' || this.value === '3' && quantityGuestsPlaceForm.value === '2' || this.value === '3' && quantityGuestsPlaceForm.value === '3') {
-    quantityGuestsPlaceForm.options[0].removeAttribute('disabled');
-    quantityGuestsPlaceForm.options[1].removeAttribute('disabled');
-    quantityGuestsPlaceForm.options[2].removeAttribute('disabled');
+    if (quantityGuestsPlaceForm.value === '0' || quantityGuestsPlaceForm.value === '3') {
+      quantityGuestsPlaceForm.value = '2';
+    }
   } else if (this.value === '3') {
     quantityGuestsPlaceForm.options[0].removeAttribute('disabled');
     quantityGuestsPlaceForm.options[1].removeAttribute('disabled');
     quantityGuestsPlaceForm.options[2].removeAttribute('disabled');
-    quantityGuestsPlaceForm.value = '3';
+    if (quantityGuestsPlaceForm.value === '0') {
+      quantityGuestsPlaceForm.value = '3';
+    }
   } else if (this.value === '100') {
     quantityGuestsPlaceForm.options[3].removeAttribute('disabled');
     quantityGuestsPlaceForm.value = '0';
